47dd75381a67aa10dc23c0e54631f0b2cdef66b2
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-12-26 15:10:06 +0530

    Implemented countLine, countWord, countCharacter in util.js

--- src/lib.js
+++ src/lib.js
@@ -1,18 +1,18 @@
-const { trim } = require("./util.js");
+const { 
+  countLine,
+  countWord,
+  countCharacter,
+} = require("./util.js");
 
 const wordCount = function(userArgs, fs) {
   const reader = fs.readFileSync;
   const filePath = userArgs[0];
   const totalContents = reader(filePath, "utf8");
 
-  const lines = totalContents.split("\n");
-  const potentialWords = lines.join(" ").split(" ");
-  const words = trim(potentialWords);
-  const characters = totalContents.split("");
+  const lineCount = countLine(totalContents) - 1;
+  const wordCount = countWord(totalContents);
+  const charCount = countCharacter(totalContents);
 
-  const lineCount = lines.length - 1;
-  const wordCount = words.length;
-  const charCount = characters.length;
   const formattedResult =
     "\t" + lineCount + "\t" + wordCount + "\t" + charCount + " " + filePath;
     
--- src/util.js
+++ src/util.js
@@ -6,6 +6,26 @@ const trim = function(sourceArray) {
   return sourceArray.filter(whitespace);
 };
 
+const countLine = function(content) {
+  return content.split("\n").length;
+};
+
+const countWord = function(content) {
+  //const replacedContent = content.replace("\n", " ");
+  const replacedContent =  content.split("\n").join(" ")
+  const potentialWords = replacedContent.split(" ");
+  const words = trim(potentialWords);
+  return words.length;
+};
+
+const countCharacter = function(content) {
+  return content.split("").length;
+};  
+
+
 module.exports = {
-  trim
+  trim,
+  countLine,
+  countWord,
+  countCharacter
 };
