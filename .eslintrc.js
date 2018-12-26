119f3114ce867ec583766e96cb8760f9f1688944
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-12-26 10:39:08 +0530

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true
+  },
+
+  "extends": "eslint:recommended",
+
+  "parserOptions": {
+    "ecmaVersion": 2015,
+    "sourceType": "module"
+  },
+  
+  "rules": {
+    "no-console": "off",
+    "indent": [ "error", 2 ],
+    "linebreak-style": [ "error", "unix" ],
+    "quotes": [ "error", "double" ],
+    "semi": [ "error", "always" ]
+  }
+
+};
