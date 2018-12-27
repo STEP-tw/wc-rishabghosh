47fc3519e8ed5f3b2874625df440849a35af25d9
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-12-27 23:12:59 +0530

    Introduced new global constants -> SPACE, doubleFile

--- scripts/appTest.js
+++ scripts/appTest.js
@@ -1,12 +1,18 @@
 const shelljs = require("shelljs");
 const { options } = require("./appTestData.js");
 
-const file1 = process.argv[2];
-const file2 = process.argv[3];
 const green = "\x1b[32m";
 const red = "\x1b[31m";
 const reset = "\x1b[0m";
+
 let message = red + "Failed" + reset;
+
+const SPACE = " ";
+
+const sampleFile1 = process.argv[2];
+const sampleFile2 = process.argv[3];
+const doubleFile = sampleFile1 + SPACE + sampleFile2;
+
 
 const shellCmd = function(commandLineArg) {
   return shelljs.exec(commandLineArg, {silent: true}).stdout;
@@ -14,13 +20,15 @@ const shellCmd = function(commandLineArg) {
 
 console.log("PLZ provide sample file", "ignore if provided\n");
 
+
+
 console.log("For Single File");
 options.map( option => {
-  const shellOut = shellCmd("wc " + option + " " + file1);
-  const userOut = shellCmd("node wc.js " + option + " " + file1);
+  const shellOut = shellCmd("wc " + option + SPACE + sampleFile1);
+  const userOut = shellCmd("node wc.js " + option + SPACE + sampleFile1);
   const formattedShellOut = shellOut.split(" ").filter(x=>x).join(" ");
   const formattedUserOut = userOut.split(/["\t", " "]/).filter(x=>x).join(" ");
-  const format = "For Format -> node wc.js " + option + " " + file1;
+  const format = "For Format -> node wc.js " + option + SPACE + sampleFile1;
   if (formattedShellOut === formattedUserOut) {
     message = green + "Passed" + reset;
   }
@@ -31,11 +39,11 @@ options.map( option => {
 
 console.log("\nFor Multiple Files");
 options.map( option => {
-  const shellOut = shellCmd("wc " + option + " " + file1 + " " + file2);
-  const userOut = shellCmd("node wc.js " + option + " " + file1 + " " + file2);
+  const shellOut = shellCmd("wc " + option + SPACE + doubleFile);
+  const userOut = shellCmd("node wc.js " + option + SPACE + doubleFile);
   const formattedShellOut = shellOut.split(" ").filter(x=>x).join(" ");
   const formattedUserOut = userOut.split(/["\t", " "]/).filter(x=>x).join(" ");
-  const format = "For Format -> node wc.js " + option + " " + file1 + " " + file2;
+  const format = "For Format -> node wc.js " + option + SPACE + doubleFile;
   if (formattedShellOut === formattedUserOut) {
     message = green + "Passed" + reset;
   }
