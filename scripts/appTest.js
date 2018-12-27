cb36633859fe78f10f3e56ae0a9398f851418975
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-12-27 22:56:33 +0530

    Modified shellCmd in appTest.js

--- scripts/appTest.js
+++ scripts/appTest.js
@@ -1,4 +1,4 @@
-const shellCmd = require("shelljs").exec;
+const shelljs = require("shelljs");
 const { options } = require("./appTestData.js");
 
 const file1 = process.argv[2];
@@ -8,12 +8,16 @@ const red = "\x1b[31m";
 const reset = "\x1b[0m";
 let message = red + "Failed" + reset;
 
+const shellCmd = function(commandLineArg) {
+  return shelljs.exec(commandLineArg, {silent: true}).stdout;
+};
+
 console.log("PLZ provide sample file", "ignore if provided\n");
 
 console.log("For Single File");
 options.map( option => {
-  const shellOut = shellCmd("wc " + option + " " + file1, {silent: true}).stdout;
-  const userOut = shellCmd("node wc.js " + option + " " + file1, {silent:true}).stdout;
+  const shellOut = shellCmd("wc " + option + " " + file1);
+  const userOut = shellCmd("node wc.js " + option + " " + file1);
   const formattedShellOut = shellOut.split(" ").filter(x=>x).join(" ");
   const formattedUserOut = userOut.split(/["\t", " "]/).filter(x=>x).join(" ");
   const format = "For Format -> node wc.js " + option + " " + file1;
@@ -27,8 +31,8 @@ options.map( option => {
 
 console.log("\nFor Multiple Files");
 options.map( option => {
-  const shellOut = shellCmd("wc " + option + " " + file1 + " " + file2, {silent: true}).output;
-  const userOut = shellCmd("node wc.js " + option + " " + file1 + " " + file2, {silent:true}).output;
+  const shellOut = shellCmd("wc " + option + " " + file1 + " " + file2);
+  const userOut = shellCmd("node wc.js " + option + " " + file1 + " " + file2);
   const formattedShellOut = shellOut.split(" ").filter(x=>x).join(" ");
   const formattedUserOut = userOut.split(/["\t", " "]/).filter(x=>x).join(" ");
   const format = "For Format -> node wc.js " + option + " " + file1 + " " + file2;
