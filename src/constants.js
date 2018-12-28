0d229e35b2c3288970a6e8c737cae117129a8225
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-12-28 10:27:53 +0530

    Introducing constants.js which holds all the contants

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const NEWLINE = "\n";
+const EMPTY_STRING = "";
+const HYPHEN = "-";
+const TAB = "\t";
+const SPACE = " ";
+
+module.exports = {
+  NEWLINE,
+  EMPTY_STRING,
+  HYPHEN,
+  TAB,
+  SPACE
+};
