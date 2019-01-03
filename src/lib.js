a0d569f161db5d15a50b304bbd1d964b3279f3a4
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-01-03 11:45:43 +0530

    extracted logic from readFile callback and implimented getDetails

--- src/lib.js
+++ src/lib.js
@@ -9,29 +9,34 @@ const countingMethods = {
   charCount: countCharacter
 };
 
+const getDetails = function (report, options, filePath, content) {
+  const eachReport = {};
+  //use forEach insted of map
+  options.map(option => {
+    const chosenMethod = countingMethods[option];
+    eachReport[option] = chosenMethod(content);
+  });
+
+  eachReport[filePath] = filePath;
+  report.push(eachReport);
+  return report;
+};
+
+
+
 const wc = function (userArgs, fs, printer) {
   const { options, filePaths } = parser(userArgs);
   const reader = fs.readFile;
   let report = [];
-
   //use forEach instead of for
   for (let filePath of filePaths) {
-
+    //make callback a function to avoid confusing
     reader(filePath, "utf8", function (error, content) {
-
-      const eachReport = {};
-      //use forEach insted of map
-      options.map(option => {
-        const chosenMethod = countingMethods[option];
-        eachReport[option] = chosenMethod(content);
-      });
-
-      eachReport[filePath] = filePath;
-      report.push(eachReport);
-
+      report  = getDetails(report, options, filePath, content);
       if (report.length === filePaths.length) {
         printer(null, formatOutput(report));
       }
+
 
     });
   }
